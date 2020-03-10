83eea1d36da4769ecef302fd120e1ab4a32c8a16
preeti1 <preeti.z747@gmail.com>
2020-03-10 22:31:56 +0530

    files added

---
+++ src/main/java/com/example/restfulservices2/restfulservices2/model/User.java
@@ -0,0 +1,41 @@
+package com.example.restfulservices2.restfulservices2.model;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@ApiModel(description = "This is user class")
+public class User {
+
+    @ApiModelProperty(notes = "This is user name")
+    private String name;
+    @ApiModelProperty(notes = "This is user age")
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
